53785d214b8d8c9a74b62a0d60893699f875aad3
La Cruz <crabraham0@gmail.com>
2025-03-06 19:58:02 +0100

    changed color scheme

--- init.lua
+++ init.lua
@@ -14,10 +14,40 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require("vim-option")
+
 require("lazy").setup("plugins")
 
+Yellow = "#bbbb00"
+White = "#ababab"
+White2 = "#dddddd"
+Grey = "#777777"
+Green = "#30ab30"
+Blue = "#0000ab"
+Blue2 = "#1020bb"
+Reddish = "#db5555"
 
+-- :TSHighlightCapturesUnder
+
+vim.api.nvim_set_hl(0, "@type.c", { fg = Yellow, bold = true})
+vim.api.nvim_set_hl(0, "@type", { fg = White2})
+vim.api.nvim_set_hl(0, "@constant", { fg = White2})
+vim.api.nvim_set_hl(0, "@constant.macro", { fg = White2, bold = true})
+vim.api.nvim_set_hl(0, "@variable", { fg = White2})
+vim.api.nvim_set_hl(0, "@number", { fg = White2})
+vim.api.nvim_set_hl(0, "@operator", { fg = White2})
+vim.api.nvim_set_hl(0, "@punctuation.bracket", { fg = White2})
+vim.api.nvim_set_hl(0, "@punctuation.delimiter", { fg = White2})
+vim.api.nvim_set_hl(0, "@keyword.directive.define", { fg = White2} )
+vim.api.nvim_set_hl(0, "@keyword.directive", { fg = White2} )
+vim.api.nvim_set_hl(0, "@function", { fg = White})
+vim.api.nvim_set_hl(0, "@function.call", { fg = White})
+vim.api.nvim_set_hl(0, "@keyword.conditional", { fg = Yellow, bold = true})
+vim.api.nvim_set_hl(0, "@keyword.return", { fg = Yellow, bold = true})
+vim.api.nvim_set_hl(0, "@keyword.repeat", { fg = Yellow, bold = true})
+vim.api.nvim_set_hl(0, "@keyword.import", { fg = Grey})
+vim.api.nvim_set_hl(0, "@comment", { fg = Reddish})
+vim.api.nvim_set_hl(0, "@string", { fg = Green})
+vim.api.nvim_set_hl(0, "@boolean", { fg = White})
 
 
